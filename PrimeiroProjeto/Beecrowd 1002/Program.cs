2be9e8a86d76e0ba6b5c95177ef1d2f5657ea45b
DeveloperFabricio <fabriciog_oliveira@outlook.com>
2023-07-10 22:44:53 -0300

    Urin 1002

---
+++ PrimeiroProjeto/Beecrowd 1002/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace curso
+{
+    class Program
+    {
+        private static string X;
+
+        static void Main(string[] args)
+        {
+
+            double n, raio, area;
+
+            n = 3.14159;
+            raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            area = n * (raio * raio);
+
+            Console.WriteLine("A=" + area.ToString("F4", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
