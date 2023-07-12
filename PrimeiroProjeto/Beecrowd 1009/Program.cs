f52768bca2edca19a4982452926d60cef9ba461d
DeveloperFabricio <fabriciog_oliveira@outlook.com>
2023-07-12 17:14:50 -0300

    Beecrowd 1009

---
+++ PrimeiroProjeto/Beecrowd 1009/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace uri1009
+{
+    class Course
+    {
+        static void Main(string[] args)
+        {
+            string vendedor;
+            double salario, horas, media;
+
+            vendedor = Console.ReadLine();
+            salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture); 
+            horas = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            media = horas * 0.15 + salario;
+
+            Console.WriteLine("TOTAL = R$ " + media.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
