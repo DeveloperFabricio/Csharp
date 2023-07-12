6d118fce14b30511b8a6137027dabb3b521aa004
DeveloperFabricio <fabriciog_oliveira@outlook.com>
2023-07-12 17:26:01 -0300

    Beecrowd 1011

---
+++ PrimeiroProjeto/Beecrowd 1011/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace uri1011
+{
+    class Course
+    {
+        static void Main(string[] args)
+        {
+            double X, pi, volume;
+
+            X = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            pi = 3.14159;
+            volume = 4 / 3.0 * pi * ( X * X * X);
+
+            Console.WriteLine("VOLUME = " + volume.ToString("F3", CultureInfo.InvariantCulture));
+        }
+    }
+}
