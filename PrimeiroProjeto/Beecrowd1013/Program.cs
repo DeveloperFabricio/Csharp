9c849017388755bd43f0b7becb6229b4ffe39f32
DeveloperFabricio <fabriciog_oliveira@outlook.com>
2023-07-12 18:17:36 -0300

    Beecrowd 1013

---
+++ PrimeiroProjeto/Beecrowd1013/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace uri1013
+{
+    class Course
+    {
+        static void Main(string[] args)
+        {
+            int A, B, C, maior, maiorXY;
+
+            string[] vet;
+
+            vet = Console.ReadLine().Split(' ');
+
+            A = int.Parse(vet[0]);
+            B = int.Parse(vet[1]);
+            C = int.Parse(vet[2]);  
+
+            maiorXY = (A + B + Math.Abs(A - B)) / 2;
+
+            maior = (maiorXY + C + Math.Abs(maiorXY - C)) / 2;
+
+            Console.WriteLine(maior + " eh o maior");
+
+        }
+    }
+}
