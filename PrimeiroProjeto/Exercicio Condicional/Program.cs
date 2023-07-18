e1d3c8a8f0aa2c4f2d1ed07feba876684255c1f3
DeveloperFabricio <fabriciog_oliveira@outlook.com>
2023-07-18 06:54:55 -0300

    Exercicio If e Else

---
+++ PrimeiroProjeto/Exercicio Condicional/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio
+{
+    class Course
+    {
+        static void Main(string[] args)
+        {
+            double nota1, nota2, resultado;
+
+            Console.WriteLine("Digite sua primeira nota: ");
+            nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Digite sua segunda nota: ");
+            nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            resultado = nota1 + nota2;
+
+            if (resultado > 60.00) {
+                Console.WriteLine("NOTA FINAL: " + resultado.ToString("F1", CultureInfo.InvariantCulture));
+                
+            }
+            else
+            {
+                Console.WriteLine("NOTA FINAL = " + resultado.ToString("F1", CultureInfo.InvariantCulture));
+                Console.WriteLine("REPROVADO");
+            }
+
+
+        }
+    }
+}
