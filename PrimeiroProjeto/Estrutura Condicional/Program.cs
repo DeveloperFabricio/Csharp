54b6ec4fc576be948298aa966e3d2d3a12c9194f
DeveloperFabricio <fabriciog_oliveira@outlook.com>
2023-07-18 06:32:23 -0300

    Estrutra Condicional

---
+++ PrimeiroProjeto/Estrutura Condicional/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace teste_ec
+{
+class Course
+{
+    static void Main(string[] args)
+    {
+
+        int horas;
+
+        Console.WriteLine("Quantas horas?");
+        horas = int.Parse(Console.ReadLine());
+
+        if (horas < 12)
+        {
+            Console.WriteLine("Bom dia!");
+        }
+        else if (horas < 18)
+        {
+            Console.WriteLine("Boa Tarde!");
+        }
+        else
+        {
+            Console.WriteLine("Boa Noite!");
+        }
+            
+        Console.ReadLine();
+    }
+}
+}
