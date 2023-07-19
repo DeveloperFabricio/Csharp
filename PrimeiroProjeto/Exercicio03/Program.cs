4deddd4edc2a01d1989dac44393f1b2f0d3a8bdb
DeveloperFabricio <fabriciog_oliveira@outlook.com>
2023-07-19 20:49:42 -0300

    Exercicio 03 C#

---
+++ PrimeiroProjeto/Exercicio03/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+using System.Threading.Tasks.Sources;
+
+namespace Exercicio03
+{
+    class Course
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+
+            String[] vet;
+
+            vet = Console.ReadLine().Split(' ');
+
+            a = int.Parse(vet[0]);
+            b = int.Parse(vet[1]);
+            c = int.Parse(vet[2]);
+
+            if (a < b && a < c)
+            {
+                Console.WriteLine("MENOR = " + a);
+            }
+            else if (b < c)
+            {
+                Console.WriteLine("MENOR = " + b);
+            }
+            else
+            {
+                Console.WriteLine("MENOR = " + c);
+            }
+        }
+    }
+}
