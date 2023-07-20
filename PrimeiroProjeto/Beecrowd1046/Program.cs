6497e1c552c079302b001fbbf9a48e14a7da3270
DeveloperFabricio <fabriciog_oliveira@outlook.com>
2023-07-20 20:15:30 -0300

    Beecrowd 1046

---
+++ PrimeiroProjeto/Beecrowd1046/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace uri1046
+{
+    class Course
+    {
+        static void Main(string[] args)
+        {
+            int x, y, duracao;
+
+            string[] vet = Console.ReadLine().Split(' ');
+            x = int.Parse(vet[0]);
+            y = int.Parse(vet[1]);
+
+            if(x < y)
+            {
+                duracao = y - x;
+            }
+            else
+            {
+                duracao = 24 - x + y;
+            }
+            Console.WriteLine("O JOGO DUROU " + duracao + " HORA(S)");
+        }
+    }
+}
