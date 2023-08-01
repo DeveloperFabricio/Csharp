c2ec73e085a876f9ccb6fb0842a3d17cc8f6f054
DeveloperFabricio <fabriciog_oliveira@outlook.com>
2023-08-01 17:29:38 -0300

    Beecrowd 1114

---
+++ PrimeiroProjeto/Beecrowd1114/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace uri1114
+{
+    class Course
+    {
+        static void Main(string[] args)
+        {
+
+            int x = int.Parse(Console.ReadLine());
+
+            while (x != 2002)
+            {
+                Console.WriteLine("Senha Invalida");
+                x = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Acesso Permitido");
+        }
+    }
+}
