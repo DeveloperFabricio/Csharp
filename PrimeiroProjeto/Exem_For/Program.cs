241d8ffb7ff8ecee2f2a83c1c19927079a7e546b
DeveloperFabricio <fabriciog_oliveira@outlook.com>
2023-08-04 19:32:54 -0300

    Exemplo For

---
+++ PrimeiroProjeto/Exem_For/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Exem_For
+{
+    class Course
+    {
+        static void Main(string[] args)
+        {
+            int N, x, soma;
+
+            N = int.Parse(Console.ReadLine());
+
+            soma = 0;
+            for ( int i=1; i<= N; i++ )
+            {
+                x = int.Parse(Console.ReadLine());
+                soma = soma + x;
+            }
+            Console.WriteLine(soma);
+            Console.ReadLine();
+        }
+    }
+}
