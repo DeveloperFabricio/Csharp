7e4ad7c9d15c415efc719460f76f0ba80fe891fc
DeveloperFabricio <fabriciog_oliveira@outlook.com>
2023-08-04 19:49:15 -0300

    Beecrowd 1078

---
+++ PrimeiroProjeto/Beecrowd1078/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace uri1078
+{
+    class Course
+    {
+        static void Main(string[] args)
+        {
+            int X, resultado;
+
+            X = int.Parse(Console.ReadLine());
+
+            for (int i=1; i<=10; i++)
+            {
+                resultado = i * X;
+                Console.WriteLine(i + " x " + X + " = " + resultado);
+            }
+        }
+    }
+}
